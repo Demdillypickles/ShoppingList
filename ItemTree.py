d8ed17932d3ba9cdfedea9f13e216ece8555fae6
Demdillypickles <46602704+Demdillypickles@users.noreply.github.com>
2020-07-04 09:09:46 -0500

    Completed updating comment headers, type hints, and docstrings

--- ItemTree.py
+++ ItemTree.py
@@ -1,24 +1,29 @@
 from typing import List, Tuple, Dict
-import neo4j
+from neo4j import GraphDatabase
 
-driver = neo4j.GraphDatabase.driver("bolt:http://localhost:7687", auth=("neo4j", "qwer1234"))
+driver = GraphDatabase.driver("bolt:http://localhost:7687", auth=("neo4j", "qwer1234"))
 
-__ReqMateral = str
+# ------------  Custom Typing Objects  ------------
+
+__ReqMaterial = str
+"""An item that is required to craft the desired item."""
 __QtyNeeded = int
-__IngredientList = List[Tuple[__ReqMateral, __QtyNeeded]]
+"""The amount of a __ReqMaterial required to fulfill a recipe."""
+__IngredientList = List[Tuple[__ReqMaterial, __QtyNeeded]]
+"""A list of tuples defining the required materials:[('ingredient1', 10), ('ingredient2', 25), (etc...)]"""
+
+# ----------  Graph Interface Functions -----------
 
 def create_new_entry(entry_name: str, ingredients: __IngredientList) -> None:
     """Add a new recipe to the graph. If existing items are referenced in the recipe,
     relationships will be connected with existing items. Items not already in the graph
-     will be added.
-     The ingredients should be organized as nested lists:
-     [(ingredient1, qty), (ingredient2, qty), (etc...)]"""
+     will be added."""
 
     with driver.session() as session:
         session.write_transaction(_add_recipe, entry_name, ingredients)
 
 
-def req_lookup(entry_name: str) -> Dict[__ReqMateral, __QtyNeeded]:
+def req_lookup(entry_name: str) -> Dict[__ReqMaterial, __QtyNeeded]:
     """Searches graph for an item by name and returns the required materials in a dict."""
     requirements = {}
     with driver.session() as session:
@@ -28,9 +33,9 @@ def req_lookup(entry_name: str) -> Dict[__ReqMateral, __QtyNeeded]:
         return requirements
 
 
-# Cypher Queries ------------------------
+# ---------------  Cypher Queries  ----------------
 
-def _add_recipe(tx, result: str, recipe: ):
+def _add_recipe(tx, result: str, recipe: __IngredientList):
     return tx.run('MERGE (item:Item {name: $result}) WITH item '
                   'FOREACH (req IN $recipe | '
                   'MERGE (i:Item {name: req[0]}) '
@@ -41,6 +46,3 @@ def _add_recipe(tx, result: str, recipe: ):
 def _get_recipe(tx, item):
     return tx.run('MATCH (n:Item {name: $item})-[req:REQUIRES]->(a) '
                   'RETURN a.name AS ingredient, req.qty AS qty', item=item)
-
-
-print(req_lookup('Antimatter'))
