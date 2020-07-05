93165ba6de260a20b6d4181214f057815d954d24
Demdillypickles <46602704+Demdillypickles@users.noreply.github.com>
2020-07-05 14:46:05 -0500

    Updated create_new_entry to support recipe as a Dict. Added interface for dumping and loading ItemTrees as JSON.

--- Cookbook/__init__.py
+++ Cookbook/__init__.py
@@ -1,7 +1,28 @@
+# Cookbook is the manager of JSON files and contains functions for loading and dumping ItemTrees
 import json
+import ItemTree
 
 
-def get_recipe(game, category, recipe):
-    with open(f"Cookbook/{game}/{category}.json") as file:
-        entry = json.load(file)[recipe]
-    return entry
+def get_cookbook(game):
+    with open(f"Cookbook/{game}.json", 'r') as file:
+        cookbook = json.load(file)
+    return cookbook
+
+
+def sorted_book(cookbook) -> dict:
+    new_book = {}
+    for key in sorted(cookbook):
+        new_book[key] = cookbook[key]
+    return new_book
+
+
+def make_cookbook(game):
+    cookbook = ItemTree.make_cookbook()
+    with open(f"Cookbook/{game}.json", 'w') as file:
+        json.dump(sorted_book(cookbook), file, indent=2)
+
+
+def load_cookbook(game):
+    cookbook = get_cookbook(game)
+    for result, recipe in cookbook.items():
+        ItemTree.create_new_entry(result, recipe)
--- ItemTree.py
+++ ItemTree.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Dict
+from typing import Dict
 from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver("bolt:http://localhost:7687", auth=("neo4j", "qwer1234"))
@@ -9,10 +9,11 @@ __ReqMaterial = str
 """An item that is required to craft the desired item."""
 __QtyNeeded = int
 """The amount of a __ReqMaterial required to fulfill a recipe."""
-__IngredientList = List[Tuple[__ReqMaterial, __QtyNeeded]]
-"""A list of tuples defining the required materials:[('ingredient1', 10), ('ingredient2', 25), (etc...)]"""
+__IngredientList = Dict[__ReqMaterial, __QtyNeeded]
+"""A dict defining the required materials:{'ingredient1': 10, 'ingredient2': 25, etc...}"""
 
 # ----------  Graph Interface Functions -----------
+
 
 def create_new_entry(entry_name: str, ingredients: __IngredientList) -> None:
     """Add a new recipe to the graph. If existing items are referenced in the recipe,
@@ -33,16 +34,35 @@ def req_lookup(entry_name: str) -> Dict[__ReqMaterial, __QtyNeeded]:
         return requirements
 
 
+def make_cookbook():
+    """Create a JSON file containing all recipes in the current graph"""
+    cookbook = {}
+    with driver.session() as session:
+        recipes = _all_recipes(session)
+        for recipe in recipes:
+            req = {recipe['material']: recipe['qty']}
+            try:
+                cookbook[recipe['result']].update(req)
+            except KeyError:
+                cookbook[recipe['result']] = req
+    return cookbook
+
+
 # ---------------  Cypher Queries  ----------------
 
 def _add_recipe(tx, result: str, recipe: __IngredientList):
     return tx.run('MERGE (item:Item {name: $result}) WITH item '
-                  'FOREACH (req IN $recipe | '
-                  'MERGE (i:Item {name: req[0]}) '
-                  'MERGE (item)-[:REQUIRES{qty:req[1]}]->(i) '
+                  'FOREACH (req IN keys($recipe) | '
+                  'MERGE (i:Item {name: req}) '
+                  'MERGE (item)-[:REQUIRES{qty: $recipe[req]}]->(i) '
                   ')', result=result, recipe=recipe)
 
 
 def _get_recipe(tx, item):
     return tx.run('MATCH (n:Item {name: $item})-[req:REQUIRES]->(a) '
                   'RETURN a.name AS ingredient, req.qty AS qty', item=item)
+
+
+def _all_recipes(tx,):
+    return tx.run('MATCH (n)-[r:REQUIRES]->(i) '
+                  'RETURN n.name AS result, r.qty AS qty, i.name AS material')
